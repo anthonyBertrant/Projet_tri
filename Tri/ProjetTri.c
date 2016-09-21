371b3911fca9ac66f5d23c4b2b5a63bcc4339d4b
BBprog <bricemaussang@gmx.fr>
2016-09-21 18:11:22 +0200

    Update ProjetTri.c

--- Tri/ProjetTri.c
+++ Tri/ProjetTri.c
@@ -9,6 +9,7 @@
 typedef int TABLEAU[MAX];
 
 #include <stdlib.h>
+#include "ExecBenchmark.h";
 
 const int Tailles[15] = {100, 500, 5000, 10000, 50000, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
 
