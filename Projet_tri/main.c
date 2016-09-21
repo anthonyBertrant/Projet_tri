02a77c7cb63d22bebe7afc0e11a9c7335dcac90b
anthony <anthony.bertrant@etu.univ-amu.fr>
2016-09-21 21:30:48 +0200

    Initial Commit

---
+++ Projet_tri/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Projet_tri
+//
+//  Created by Anthony Bertrant on 21/09/2016.
+//  Copyright © 2016 AnthonyBertrant_BriceMaussang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
