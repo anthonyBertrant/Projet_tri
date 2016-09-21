0f4f806fc89ad58e8d00d5d479b1b87473759be5
BBprog <bricemaussang@gmx.fr>
2016-09-21 18:12:34 +0200

    Update ExecuteBenchmark.c

--- Tri/ExecuteBenchmark.c
+++ Tri/ExecuteBenchmark.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+#include "TriInsertSeq.h"
+#include "TriInsertDicho.h"
+#include "TriSelecPerm.h"
+#include "TriBulles.h"
+#include "TriFusion.h"
+#include "TriRapide.h"
+#include "TriArbreBin.h"
+#include "TriTas.h"
+
 /*
 execute les tests
 */
