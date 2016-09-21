c0a5306f6431b5aef18eeb2fc865450d918792d7
anthony <anthony.bertrant@etu.univ-amu.fr>
2016-09-21 18:59:16 +0200

    vvv

--- Tri/ExecuteBenchmark.c
+++ Tri/ExecuteBenchmark.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
 
-#include "TriInsertSeq.h"
-#include "TriInsertDicho.h"
-#include "TriSelecPerm.h"
-#include "TriBulles.h"
-#include "TriFusion.h"
-#include "TriRapide.h"
-#include "TriArbreBin.h"
-#include "TriTas.h"
-
-const int Tailles[15] = {100, 500, 5000, 10000, 50000, 100000,
+const int Ksizes[15] = {100, 500, 5000, 10000, 50000, 100000,
                          200000, 300000, 400000, 500000, 600000,
                          700000, 800000, 900000, 1000000};
 
-<<<<<<< HEAD
-void ExecuteBenchmark(){
+
+void ExecuteBenchmark()
+{
 /*
 execute les tests
 */
 }
-=======
-void ExecuteBenchmark()
-/*
-execute les tests
-*/
->>>>>>> master
