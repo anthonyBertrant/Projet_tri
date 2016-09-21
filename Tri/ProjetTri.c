d8260ba263d34597fd88ecd71b170c5b81b29970
BBprog <bricemaussang@gmx.fr>
2016-09-21 18:30:34 +0200

    Update ProjetTri.c

--- Tri/ProjetTri.c
+++ Tri/ProjetTri.c
@@ -5,13 +5,8 @@
 //  Created by Anthony Bertrant on 21/09/2016.
 //  Copyright © 2016 AnthonyBertrant_BriceMaussang. All rights reserved.
 //
-#define MAX 1000000
-typedef int TABLEAU[MAX];
-
 #include <stdlib.h>
 #include "ExecBenchmark.h";
-
-const int Tailles[15] = {100, 500, 5000, 10000, 50000, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000};
 
 
 int main(){
