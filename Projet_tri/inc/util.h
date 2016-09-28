3fc6b9194a74a549d32f7b87b368560bd185089a
BBprog <bricemaussang@gmx.fr>
2016-09-28 17:38:19 +0200

    Add files via upload

--- Projet_tri/inc/util.h
+++ Projet_tri/inc/util.h
@@ -2,13 +2,24 @@
 #ifndef HEADERFILE_H
 #define HEADERFILE_H
 
-#define MAX 1000000
-typedef int TABLEAU[MAX];
-
 void GenerateRandTab(TABLEAU t, int tabSize);
+/* spécification: 
+Rempli le tableau passé en paramètre avec des valeurs aléatoires.
+	-en entrée: tabSize > 0.
+	-en sortie: t[0..tabSize] rempli avec avec des entiers générés aléatoirement.
+*/
 
-void WriteResultInFile(int result, int taille, const char *filename);
+void WriteResultInFile(int result, int size, const char *filename);
+/* spécification: 
 
-void Swap(TABLEAU t, int indice1, int indice2);
+*/
+
+void Swap(TABLEAU t, int x, int y);
+/* spécification:
+Echange les valeurs présentes dans le tableau passé en paramètre aux indices également passés en paramètre. 
+	-en entrée: 0 <= x < n, 0 <= y < n avec n étant la taille de t.
+	-en sortie: les deux valeurs aux indices x et y ont été échangées.
+*/
+
 
 #endif
