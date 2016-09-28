1f780ada3683b198cb1eae1238cd735c6a4aa711
BBprog <bricemaussang@gmx.fr>
2016-09-28 18:44:57 +0200

    Add files via upload

--- Tri/ProjetTri.c
+++ Tri/ProjetTri.c
@@ -5,22 +5,41 @@
 //  Created by Anthony Bertrant on 21/09/2016.
 //  Copyright © 2016 AnthonyBertrant_BriceMaussang. All rights reserved.
 //
+#include <stdio.h>
 #include <stdlib.h>
-#include "ExecBenchmark.h";
 
-#include "TriInsertSeq.h"
-#include "TriInsertDicho.h"
-#include "TriSelecPerm.h"
-#include "TriBulles.h"
-#include "TriFusion.h"
-#include "TriRapide.h"
-#include "TriArbreBin.h"
-#include "TriTas.h"
+#define MAX 1000000
+typedef int TABLEAU[MAX];
 
-#define MAX 100000;
-typedef int[MAX] TABLEAU;
+void TriRapide(int t){
+    printf("rapide\n");
+}
+    
+
+void TriFusion(int t){
+	printf("fusion\n");
+}
+
+
+typedef void (*p)(); // pointeur sur fonction retournant un int et en prenant 2 en parametres.
+ 
+
+#define TAB_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
 int main()
 {
+	p flags[]= {
+	   &TriFusion,
+	   &TriRapide,   
+	};
+
+	// on parcours notre tableau jusqu'a trouver la bonnne fonction. ici appeleras la fonction is_i
+	int i = 0;
+	while (i < TAB_SIZE(flags))
+	{
+		flags[i](0);
+		i++;
+	}
+	
 	return(0);
 }
