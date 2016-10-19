1e1a868cc41bc8e6a85262065fbedaa776f7fc4f
BBprog <bricemaussang@gmx.fr>
2016-10-19 10:56:55 +0200

    Update TriArbreBin.c

--- Tri/TriArbreBin.c
+++ Tri/TriArbreBin.c
@@ -11,9 +11,65 @@
 #define MAX 1000000
 typedef int TABLEAU[MAX];
 
+typedef struct node {
+  int value;
+  struct node *left;
+  struct node *right;
+} node;
 
-void TriArbreBin(TABLEAU t)
+void afficherArbre(node *tree)
 {
+  if(!tree) return;
+  if (tree->left)  afficherArbre(tree->left);
+  printf("Cle = %d\n", tree->value);
+  if (tree->right) afficherArbre(tree->right);
+}
 
+void addNode(node **tree, int value)
+{ 
+  node *tmpTree = *tree;
+  
+  node *elem = malloc(sizeof(node));
+  elem->value = value;
+  elem->left = NULL;
+  elem->right = NULL;
+  
+  if (tmpTree == NULL)
+  {
+    tree = elem;
+  }
+  else
+    while (tmpTree)
+    {
+      tmpNode = tmpTree;
+      if(value > tmpTree->value )
+      {
+          tmpTree = tmpTree->right;
+          if(!tmpTree) tmpNode->right = elem;
+      }
+      else
+      {
+          tmpTree = tmpTree->left;
+          if(!tmpTree) tmpNode->left = elem;
+      }
+    }
+  }
+}
+
+node* construireArbre(TABLEAU t, int size)
+{
+  node *arbre = NULL;
+  
+  int i;
+  for (i = 0; i < size; ++i) 
+    addNode(&arbre, t[i]);
+  
+  return arbre;
+}
+
+void TriArbreBin(TABLEAU t, int size)
+{
+  node *arbre = construireArbre(TABLEAU t, int size);
+  afficherArbre(arbre);
 }
 
